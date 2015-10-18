0082afc64dc934d254c9b168e6ecd89cb5d060b7
Felix S. Klock II <pnkfelix@pnkfx.org>
2015-10-18 12:27:07 +0200

    expanded set of inputs to demo_codegen.
    looking pretty good, but some details w.r.t. \empty and end-of-stream
    handling still need to be addressed.

--- src/gll/demo_codegen.rs
+++ src/gll/demo_codegen.rs
@@ -1,51 +1,36 @@
 
 
-use grammar::demo_grammar;
+use grammar::{tiny_grammar, demo_grammar};
 use codegen::rust::RustBackend;
 use gll::codegen::{Backend, Codegen};
 
 #[test]
 fn demo_1() {
-    let g = demo_grammar();
-    let g = g.tag_nonterminals();
-    let mut rb: RustBackend = Backend::new(&g);
-    let mut cg = Codegen { backend: &mut rb, grammar: &g };
-    for (i, rule) in g.rules.iter().enumerate() {
-        // FIXME: make `fn on_rule` take a `&Rule` instead of cloning.
-        let (c, blocks) = cg.on_rule(rule.clone());
-        let l_a = cg.backend.nonterm_label(rule.left);
-        let b = cg.backend.block(l_a, c);
-        let prefix = format!("i={}: ", i);
-        println!("{}{}", prefix, b.render());
-        for _ in 0..prefix.len() {
-            print!(" ");
+    for (name, g) in vec![("a", tiny_grammar::a()),
+                          ("ab", tiny_grammar::ab()),
+                          ("a|b", tiny_grammar::a_or_b()),
+                          ("a*", tiny_grammar::a_star()),
+                          ("(a|b)* ∪ (a|c)*d", demo_grammar()),
+                          ] {
+        println!("grammar for {}", name);
+        println!("");
+        let g = g.tag_nonterminals();
+        let mut rb: RustBackend = Backend::new(&g);
+        let mut cg = Codegen { backend: &mut rb, grammar: &g };
+        for (i, rule) in g.rules.iter().enumerate() {
+            // FIXME: make `fn on_rule` take a `&Rule` instead of cloning.
+            let (c, blocks) = cg.on_rule(rule.clone());
+            let l_a = cg.backend.nonterm_label(rule.left);
+            let b = cg.backend.block(l_a, c);
+            let prefix = format!("i={}: ", i);
+            println!("{}{}", prefix, b.render());
+            let blocks: String = blocks.iter()
+                .map(|b|b.render() + "\n")
+                .collect();
+            println!("{}", blocks);
         }
-        let blocks: String = blocks.iter()
-            .map(|b|b.render() + "\n")
-            .collect();
-        println!("{}", blocks);
     }
 
-    // FIXME: something seems off about the code-generation,
-    // in particular I find this suspect: *---------------+
-    //                 //                                 |
-    // L::R_A_1 => {   //                                 |
-    //     self.pop(); // <--- what is this doing here? --+
-    //     if self.i_in(&['a','b','c','d',]) {
-    //         self.create(L::R_S_1);
-    //         goto!( L::L_S );
-    //     } else {
-    //         goto!( L::_0 );
-    //     }
-    //     if self.i_in(&['d']) {
-    //         self.i.incr();
-    //     } else {
-    //         goto!( L::_0 );
-    //     }
-    //     goto!( L::_0 );
-    // }
-
-
     // (uncomment to show the output above)
-    // assert!(false);
+    assert!(false);
 }
