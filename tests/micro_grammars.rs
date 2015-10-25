ee474770ce60b805f30a12651cb529ca70b7844f
Felix S. Klock II <pnkfelix@pnkfx.org>
2015-10-25 13:52:47 +0100

    Update micro_grammars test to run `cargo update` before `cargo test`.

--- tests/micro_grammars.rs
+++ tests/micro_grammars.rs
@@ -1,13 +1,25 @@
 use std::process::{Command, exit};
 
 fn main() {
+   let output = Command::new("cargo")
+        .arg("update")
+        .current_dir("tests/micro_grammars")
+        .output()
+        .unwrap();
+    println!("cargo update stdout: {}", String::from_utf8_lossy(&output.stdout));
+    println!("");
+    println!("cargo update stderr: {}", String::from_utf8_lossy(&output.stderr));
+    if !output.status.success() {
+        exit(output.status.code().unwrap_or(1));
+    }
+
     let output = Command::new("cargo")
         .arg("test")
         .current_dir("tests/micro_grammars")
         .output()
         .unwrap();
-    println!("stdout: {}", String::from_utf8_lossy(&output.stdout));
+    println!("cargo test stdout: {}", String::from_utf8_lossy(&output.stdout));
     println!("");
-    println!("stderr: {}", String::from_utf8_lossy(&output.stderr));
+    println!("cargo test stderr: {}", String::from_utf8_lossy(&output.stderr));
     exit(output.status.code().unwrap_or(1));
 }
