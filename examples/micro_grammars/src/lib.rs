0d9f877ad15e775b8e6fd494de24f11271183758
Felix S. Klock II <pnkfelix@pnkfx.org>
2015-10-25 10:21:48 +0100

    added test of `"ad"` on `S`, and refactored test infrastructure.

--- examples/micro_grammars/src/lib.rs
+++ examples/micro_grammars/src/lib.rs
@@ -13,38 +13,46 @@ mod support;
 
 mod test_a {
     use pgy_runtime::graph::Graph;
-    use super::support::DemoContext;
+    use super::support::{DemoContext, ParseError, Success};
     use super::parse_a::{parse, nonterm};
 
-    #[test]
-    fn test_a_a() {
-        let input = "a";
+    fn demo(input: &str) -> Result<Success, ParseError> {
         let g = Graph::new();
         let mut c = DemoContext::new(input.as_bytes(), &g);
-        parse(&mut c, nonterm::S).unwrap();
+        parse(&mut c, nonterm::S)
     }
 
     #[test]
-    fn test_a_b() {
-        let input = "b";
-        let g = Graph::new();
-        let mut c = DemoContext::new(input.as_bytes(), &g);
-        parse(&mut c, nonterm::S).unwrap_err();
+    fn test_a() {
+        demo("a").unwrap();
+    }
+
+    #[test]
+    fn test_b() {
+        demo("b").unwrap_err();
     }
 }
 
 mod parse_S;
 mod test_S {
     use pgy_runtime::graph::Graph;
-    use super::support::DemoContext;
+    use super::support::{DemoContext, ParseError, Success};
     use super::parse_S::{parse, nonterm};
 
-    #[test]
-    fn test_a_a() {
-        let input = "a";
+    fn demo(input: &str) -> Result<Success, ParseError> {
         let g = Graph::new();
         let mut c = DemoContext::new(input.as_bytes(), &g);
-        parse(&mut c, nonterm::S).unwrap();
+        parse(&mut c, nonterm::S)
+    }
+
+    #[test]
+    fn test_a() {
+        demo("a").unwrap();
+    }
+
+    #[test]
+    fn test_ad() {
+        demo("ad").unwrap();
     }
 
 }
